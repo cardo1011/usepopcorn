b55426a316929926ab6e629c79fc88d43bf019d4
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-01-31 12:34:08 -0800

    created a very reusable StarRating component with default props and set proptypes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating maxRating={"string"} />
+    <StarRating
+      size={124}
+      color="red"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
